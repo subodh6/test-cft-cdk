3493b828bb72dab1b24586c4a942b7f7e43ea4d5
ankitanagarale <131234134+ankitanagarale@users.noreply.github.com>
2024-10-15 11:25:17 +0530

    Update application_start.sh

--- aws/infra/codepipeline/application_start.sh
+++ aws/infra/codepipeline/application_start.sh
@@ -4,7 +4,7 @@ echo $TOMCAT_HOME
 sudo $TOMCAT_HOME/bin/shutdown.sh
 sleep 60
 sudo rm -rf $TOMCAT_HOME/webapps/*
-sudo mv /tmp/cas-scheduler.war $TOMCAT_HOME/webapps/ROOT.war
+sudo cp /tmp/cas-scheduler.war $TOMCAT_HOME/webapps/ROOT.war
 # sudo cp -R /tmp/*.war $TOMCAT_HOME/webapps
 sudo $TOMCAT_HOME/bin/startup.sh
 
