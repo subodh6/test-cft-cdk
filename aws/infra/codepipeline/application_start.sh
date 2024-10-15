5c6fe8aa51105324c751282216743ef326b0488f
ankitanagarale <131234134+ankitanagarale@users.noreply.github.com>
2024-10-15 12:30:57 +0530

    Update application_start.sh

--- aws/infra/codepipeline/application_start.sh
+++ aws/infra/codepipeline/application_start.sh
@@ -2,14 +2,14 @@
 export TOMCAT_HOME="/opt/apache-tomcat-9.0.96"
 echo $TOMCAT_HOME
 sudo $TOMCAT_HOME/bin/shutdown.sh
-# sleep 60
+sleep 60
 sudo rm -rf $TOMCAT_HOME/webapps/*
 sudo cp /tmp/cas-scheduler.war $TOMCAT_HOME/webapps/ROOT.war
 # sudo cp -R /tmp/*.war $TOMCAT_HOME/webapps
 sudo $TOMCAT_HOME/bin/startup.sh
 
 # Wait for Tomcat to fully stop (adjust the sleep duration based on your application's shutdown time)
-# sleep 60
+sleep 60
 
 # Start Tomcat
 # sudo $TOMCAT_HOME/bin/startup.sh
